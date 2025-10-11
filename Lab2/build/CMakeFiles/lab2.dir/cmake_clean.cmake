918fbdde122f238f587d1e777299a49faf7cb764
Ivan Zhavner <zhavnervan@gmail.com>
2025-10-11 17:22:52 +0300

    Cmake build

---
+++ Lab2/build/CMakeFiles/lab2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2.dir/Lab2.cpp.obj"
+  "CMakeFiles/lab2.dir/Lab2.cpp.obj.d"
+  "lab2.exe"
+  "lab2.exe.manifest"
+  "lab2.pdb"
+  "liblab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
